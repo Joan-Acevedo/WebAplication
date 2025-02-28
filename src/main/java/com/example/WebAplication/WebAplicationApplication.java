c402860af1186f0683e6df7349b1f815540aa5f8
unknown <joan.acevedo@labinfo.is.escuelaing.edu.co>
2025-02-28 11:29:17 -0500

    first commit

---
+++ src/main/java/com/example/WebAplication/WebAplicationApplication.java
@@ -0,0 +1,16 @@
+package com.example.WebAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAplicationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WebAplicationApplication.class, args);
+
+		System.out.println("Hellor world");
+	}
+
+}
---
+++ src/test/java/com/example/WebAplication/WebAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
